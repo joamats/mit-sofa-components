205b023e743ee93077739d7377eb30dd0d917712
João Matos <joaocarlosm00@gmail.com>
2022-12-16 17:34:11 -0500

    Add MIMIC SQL query and code to run GCP's query

---
+++ src/py/get_data.py
@@ -0,0 +1,65 @@
+import argparse
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+tqdm.pandas()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dataset",
+                    default="MIMIC",
+                    help="Insert the dataset to work with")
+
+    return parser.parse_args()
+
+
+def run_query(sql_query_path, destination_path):
+
+    # Access data using Google BigQuery.
+    import os
+    from dotenv import load_dotenv
+
+    # Load env file 
+    load_dotenv()
+
+    # Get GCP's secrets
+    KEYS_FILE = os.getenv("KEYS_FILE")
+    PROJECT_ID = os.getenv("PROJECT_ID")
+
+    # Set environment variables
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = KEYS_FILE
+
+    # Establish connection with BigQuery
+    from google.cloud import bigquery
+    BigQuery_client = bigquery.Client()
+
+    # Read query
+    with open(sql_query_path, 'r') as fd:
+        query = fd.read()
+
+    # Replace the project id by the coder's project id in GCP
+    my_query = query.replace("physionet-data", PROJECT_ID).replace("db_name", PROJECT_ID, -1)
+
+    # Make request to BigQuery with our query
+    df = BigQuery_client.query(my_query).to_dataframe()
+
+    # Save to CSV
+    df.to_csv(destination_path)
+
+    return df
+    
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    # First, let's fetch the data
+
+    if args.dataset == "MIMIC":
+        run_query("src/sql/MIMIC_cohort.sql",
+                  "data/MIMIC_data.csv")
+
+    elif args.dataset == "eICU":
+        run_query("src/sql/eICU_cohort.sql",
+                  "data/eICU_data.csv")
---
+++ src/sql/MIMIC_cohort.sql
@@ -0,0 +1,158 @@
+WITH
+  cabg_adm AS (
+    SELECT DISTINCT hadm_id AS cabg_id
+    FROM `physionet-data.mimiciv_hosp.drgcodes`
+    WHERE drg_code IN ("231", "232" ,"233" , "234" ,"235" ,"236")
+)
+, cirrhosis AS (
+    SELECT DISTINCT hadm_id AS cirrhosis_id
+    FROM `physionet-data.mimiciv_hosp.diagnoses_icd`
+    WHERE icd_code IN ("K7030","K7031", "K717", "K743", "K744", "K745", "K7460", "K7469","5712", "5715", "5716")
+)
+, esrd AS (
+    SELECT DISTINCT hadm_id AS ESRD_id
+    FROM `physionet-data.mimiciv_hosp.diagnoses_icd`
+    WHERE icd_code IN ("N186", "5856")
+)
+, maxhr AS (
+    SELECT stay_id,
+           max(hr) AS max_hr
+    FROM `physionet-data.mimiciv_derived.sofa`
+    GROUP BY stay_id
+)
+, sofa24 AS (
+    SELECT stay_id,
+           respiration_24hours as resp_24,
+           coagulation_24hours as coag_24,
+           liver_24hours as liver_24,
+           cardiovascular_24hours as cv_24,
+           cns_24hours as cns_24,
+           renal_24hours as renal_24
+    FROM `physionet-data.mimiciv_derived.sofa`
+    WHERE hr = 24
+)
+, sofa168 AS (
+    SELECT stay_id,
+           respiration_24hours AS resp_168,
+           coagulation_24hours AS coag_168,
+           liver_24hours AS liver_168,
+           cardiovascular_24hours AS cv_168,
+           cns_24hours AS cns_168,
+           renal_24hours AS renal_168
+    FROM `physionet-data.mimiciv_derived.sofa`
+    WHERE hr = 168
+)
+, sofalast AS (
+    SELECT s1.stay_id,
+           maxhr.max_hr,
+           respiration_24hours AS resp_last,
+           coagulation_24hours AS coag_last,
+           liver_24hours AS liver_last,
+           cardiovascular_24hours AS cv_last,
+           cns_24hours AS cns_last,
+           renal_24hours AS renal_last
+    FROM `physionet-data.mimiciv_derived.sofa` AS s1
+    RIGHT JOIN maxhr
+    ON s1.hr=maxhr.max_hr
+    AND s1.stay_id = maxhr.stay_id
+)
+, vent24 AS (
+    SELECT s.stay_id,
+           v.ventilation_status AS vent_24
+    FROM (
+        SELECT *
+        FROM `physionet-data.mimiciv_derived.sofa`
+        WHERE hr=24
+    )  AS s
+    INNER JOIN (
+        SELECT *
+        FROM `physionet-data.mimiciv_derived.ventilation`
+        WHERE ventilation_status != "None"
+    ) AS v
+    ON s.stay_id = v.stay_id
+    AND s.starttime >= DATETIME_TRUNC(v.starttime, HOUR)
+    AND s.endtime <= DATETIME_TRUNC(v.endtime, HOUR)
+)
+, vent168 AS (
+    SELECT s.stay_id,
+           v.ventilation_status AS vent_168
+    FROM (
+        SELECT *
+        FROM `physionet-data.mimiciv_derived.sofa`
+        WHERE hr = 168
+    ) AS s
+    INNER JOIN (
+        SELECT *
+        FROM `physionet-data.mimiciv_derived.ventilation`
+        WHERE ventilation_status != "None"
+    ) AS v
+    ON s.stay_id = v.stay_id
+    AND s.starttime >= DATETIME_TRUNC(v.starttime, HOUR)
+    AND s.endtime<=DATETIME_TRUNC(v.endtime, HOUR)
+)
+, ventlast AS (
+    SELECT v.stay_id,
+           v.ventilation_status AS vent_last
+    FROM (
+        SELECT sf.stay_id,
+               sf.starttime,
+               sf.endtime,
+               sf.hr
+        FROM `physionet-data.mimiciv_derived.sofa` AS sf 
+        INNER JOIN maxhr
+        ON maxhr.stay_id = sf.stay_id
+        AND maxhr.max_hr = sf.hr + 24
+    ) AS s
+    LEFT JOIN (
+        SELECT *
+        FROM `physionet-data.mimiciv_derived.ventilation`
+        WHERE ventilation_status = "InvasiveVent"
+    ) AS v 
+    ON s.stay_id = v.stay_id
+    AND s.starttime <= v.endtime
+)
+, adm AS (
+    SELECT hadm_id,
+           deathtime,
+           admission_type,
+           admission_location,
+           discharge_location
+    FROM `physionet-data.mimiciv_hosp.admissions`
+)
+
+SELECT DISTINCT *
+FROM  `physionet-data.mimiciv_derived.icustay_detail` AS cohort 
+
+LEFT JOIN sofa24
+ON cohort.stay_id = sofa24.stay_id 
+
+LEFT JOIN sofa168
+ON cohort.stay_id = sofa168.stay_id
+
+LEFT JOIN sofalast
+ON cohort.stay_id = sofalast.stay_id
+
+LEFT JOIN vent24
+ON cohort.stay_id = vent24.stay_id
+
+LEFT JOIN vent168
+ON cohort.stay_id = vent168.stay_id
+
+LEFT JOIN ventlast
+ON ventlast.stay_id = cohort.stay_id
+
+LEFT JOIN adm
+ON cohort.hadm_id = adm.hadm_id
+
+LEFT JOIN physionet-data.mimiciv_derived.sepsis3
+AS s3
+ON cohort.stay_id = s3.stay_id
+
+LEFT JOIN cabg_adm
+ON cohort.hadm_id = cabg_adm.cabg_id
+
+LEFT JOIN cirrhosis
+ON cohort.hadm_id = cirrhosis.cirrhosis_id
+
+LEFT JOIN esrd
+ON cohort.hadm_id = esrd.esrd_id
